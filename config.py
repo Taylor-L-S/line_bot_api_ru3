8e015e01d9f151d4f5d6dcd7341bc3354aa10d2e
Taylor-L-S <104907080+Taylor-L-S@users.noreply.github.com>
2023-09-05 21:08:46 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+
+class Config:
+
+    CHANNEL_ACCESS_TOKEN = 'DX8K+3OThP2vbtYoebijLy7gosZVVSDxZAVVfvNilq6+t4yZ10g1HK74dMF3lglMlOXdSO7e9uFH4306LoYVEDQ+SW0web3KggvUFS94ciyQIX4aGgSBi8UicNe2Nr6p9KVMAlIvKGRPj1BB1YF+agdB04t89/1O/w1cDnyilFU='
+    CHANNEL_SECRET = '880545b3fef970a1460a69447feee723'
+
+    LINE_PAY_ID = '2000641146'
+    LINE_PAY_SECRET = '88ca40bfde198bc29b8c442e4e34df4f'
+
+    STORE_IMAGE_URL = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcars.usnews.com%2Fcars-trucks%2Fadvice%2Fbest-commuter-cars&psig=AOvVaw3j90I2fZnFl8bB-FCyWfbY&ust=1694001726275000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCKDgz6y2k4EDFQAAAAAdAAAAABAD'
